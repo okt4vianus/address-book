2ac073c9817d53745a5d1e1ae63f515740532172
Oktavianus R. Tasak <okt4vianus+git@gmail.com>
2024-12-12 22:18:05 +0800

    add method math and array.find

--- index.js
+++ index.js
@@ -1,146 +1,197 @@
-const contacts = [
+const dataContacts = [
   {
-    id: 1,
+    id: 4821,
     fullName: "Alice Johnson",
     company: "Tech Solutions Inc.",
     email: "alice.johnson@techsolutions.com",
     phone: "+62 123-456-7890",
-    dob: "1985-03-15",
+    birthdate: "1985-03-15",
   },
   {
-    id: 2,
+    id: 1954,
     fullName: "Bob Smith",
     company: "Creative Designs Ltd.",
     email: "bob.smith@creativedesigns.com",
     phone: "+62 234-567-8901",
-    dob: "1990-07-22",
+    birthdate: "1990-07-22",
   },
   {
-    id: 3,
+    id: 8346,
     fullName: "Agus Dewantoro",
     company: null,
     email: null,
     phone: "+62 ",
-    dob: null,
+    birthdate: null,
   },
   {
-    id: 4,
+    id: 6732,
     fullName: "Diana Williams",
     company: "Marketing Experts",
     email: "diana.williams@marketingexperts.com",
     phone: "+62 456-789-0123",
-    dob: "1992-05-04",
+    birthdate: "1992-05-04",
   },
   {
-    id: 5,
+    id: 2491,
     fullName: "Eve Davis",
     company: "Finance Solutions Group",
     email: "eve.davis@financesolutions.com",
     phone: "+62 567-890-1234",
-    dob: "1987-02-18",
+    birthdate: "1987-02-18",
   },
 ];
 
-module.exports = contacts; // Export the contacts array
+// Declare all function
 
-//Declare all function
-
-// Function cek id in contacts
-function cekId(object, id) {
-  if (id <= object.length) {
-    return true;
-  } else false;
+// Function check id in data contacts
+function checkId(contacts, id) {
+  if (contacts.find((contact) => contact.id === id)) {
+    return contacts.find((contact) => contact.id === id);
+  } else console.log(`Contact with ID ${id} not found.`);
 }
 
-// Function add data in contacts
-function addContact(object, data) {
-  object.push(data);
-  index = object.length - 1;
+// Function to generate a unique ID
+function generateId(params) {
+  return Math.floor(Math.random() * 9000 + 1000);
+}
+
+// Function add data in data contacts
+function addContact(contacts, newContact) {
+  const newContactWithId = {
+    id: generateId(),
+    fullName: newContact.fullName,
+    company: newContact.company,
+    email: newContact.email,
+    phone: newContact.phone,
+    birthdate: newContact.birthdate,
+  };
+  contacts.push(newContactWithId);
+  const lastContactIndex = contacts.length - 1;
   console.log(`
   Add contact successfully, details of the contact:
   _________________________________________________
 
-  ID            : ${object[index].id}
-  Full Name     : ${object[index].fullName}
-  Company       : ${object[index].company}
-  Email         : ${object[index].email}
-  Phone Number  : ${object[index].phone}
-  Date of Birth : ${object[index].dob}
+  ID        : ${contacts[lastContactIndex].id}
+  Full Name : ${contacts[lastContactIndex].fullName}
+  Company   : ${contacts[lastContactIndex].company}
+  Email     : ${contacts[lastContactIndex].email}
+  Phone     : ${contacts[lastContactIndex].phone}
+  Birthdate : ${contacts[lastContactIndex].birthdate}
   `);
 }
 
 // Function find data by id in contacts
-function findContactById(object, index) {
-  if (cekId(object, index)) {
-    index -= 1;
+function findContactById(contacts, id) {
+  const contact = contacts.find((contact) => contact.id === id);
+
+  if (contact) {
     console.log(`
-  Find contact of id: ${object[index].id} successfully, details of the contact:
+  Find contact of ID: ${contact.id} successfully, details of the contact:
   ___________________________________________________________
 
-  ID            : ${object[index].id}
-  Full Name     : ${object[index].fullName}
-  mail          : ${object[index].email}
-  Company       : ${object[index].company}
-  Phone Number  : ${object[index].phone}
-  Date of Birth : ${object[index].dob}
+  ID        : ${contact.id}
+  Full Name : ${contact.fullName}
+  mail      : ${contact.email}
+  Company   : ${contact.company}
+  Phone     : ${contact.phone}
+  Birthdate : ${contact.birthdate}
   `);
-  } else console.log("ID is invalid");
+  } else console.log(`  Find contact of ID ${id} not found.`);
 }
 
-// Function to remove contact by id
-function removeContactById(object, index) {
-  if (cekId(object, index)) {
-    index -= 1;
+// Function to remove contact last input
+function removeLastContact(contacts) {
+  if (contacts.length > 0) {
+    const lastContactIndex = contacts.length - 1;
     console.log(`
   Contact removed successfully, details of the removed contact:
   _____________________________________________________________
 
-  ID            : ${object[index].id}
-  Full Name     : ${object[index].fullName}
-  Company       : ${object[index].company}
-  Email         : ${object[index].email}
-  Phone Number  : ${object[index].phone}
-  Date of Birth : ${object[index].dob}
+  ID        : ${contacts[lastContactIndex].id}
+  Full Name : ${contacts[lastContactIndex].fullName}
+  Company   : ${contacts[lastContactIndex].company}
+  Email     : ${contacts[lastContactIndex].email}
+  Phone     : ${contacts[lastContactIndex].phone}
+  Birthdate : ${contacts[lastContactIndex].birthdate}
   `);
-    object.splice(index, 1);
-  } else console.log("Contact not found");
-}
-
-// Function to display all data in contacts
-function displayContacts(object) {
-  console.log(`
-  DISPLAY ALL CONTACTS:
-
-  ID | Full Name ${" ".repeat(10)} | Company ${" ".repeat(
-    20
-  )}| Email ${" ".repeat(30)} | Phone Number ${" ".repeat(5)} | Date of Birth
-  ________________________${"_".repeat(107)}`);
-  for (let i = 0; i < object.length; i++) {
-    let lenname = object[i].fullName ? 19 - object[i].fullName.length : 15;
-    let lencompany = object[i].company ? 26 - object[i].company.length : 22;
-    let lenemail = object[i].email ? 35 - object[i].email.length : 31;
-    let lenphone = object[i].phone ? 17 - object[i].phone.length : 12;
-    console.log(`
-  ${object[i].id}  | ${object[i].fullName} ${" ".repeat(lenname)} | ${
-      object[i].company
-    } ${" ".repeat(lencompany)} | ${object[i].email} ${" ".repeat(
-      lenemail
-    )} | ${object[i].phone} ${" ".repeat(lenphone)} | ${object[i].dob}`);
+    contacts.pop();
+  } else {
+    console.log(`  No contact to remove due array is empty`);
   }
 }
 
+// Function to display all data in contacts
+function displayContacts(contacts) {
+  // Display as column`)
+  if (contacts.length > 0) {
+    console.log(`
+  DISPLAY ALL CONTACTS - ${contacts.length} records
+  _________________________________________________`);
+
+    for (let index = 0; index < contacts.length; index++) {
+      console.log(`  ID        : ${contacts[index].id}
+  Full Name : ${contacts[index].fullName}
+  Company   : ${contacts[index].company}
+  Email     : ${contacts[index].email}
+  Phone     : ${contacts[index].phone}
+  Birthdate : ${contacts[index].birthdate}
+  `);
+    }
+  } else {
+    console.log(`
+  DISPLAY ALL CONTACTS - ${contacts.length} records
+  _________________________________________________
+  No record found
+    `);
+  }
+
+  // Display as row
+  //   console.log(`
+  //   DISPLAY ALL CONTACTS:
+  //   ID | Full Name ${" ".repeat(10)} | Company ${" ".repeat(
+  //     20
+  //   )}| Email ${" ".repeat(30)} | Phone ${" ".repeat(12)} | Birthdate
+  //   ________________________${"_".repeat(107)}`);
+  //   for (let index = 0; index < contacts.length; index++) {
+  //     let spacingName = contacts[index].fullName
+  //       ? 19 - contacts[index].fullName.length
+  //       : 15;
+  //     let spacingCompany = contacts[index].company
+  //       ? 26 - contacts[index].company.length
+  //       : 22;
+  //     let spacingEmail = contacts[index].email
+  //       ? 35 - contacts[index].email.length
+  //       : 31;
+  //     let spacingPhone = contacts[index].phone
+  //       ? 17 - contacts[index].phone.length
+  //       : 12;
+  //     console.log(`
+  //   ${contacts[index].id}  | ${contacts[index].fullName} ${" ".repeat(
+  //       spacingName
+  //     )} | ${contacts[index].company} ${" ".repeat(spacingCompany)} | ${
+  //       contacts[index].email
+  //     } ${" ".repeat(spacingEmail)} | ${contacts[index].phone} ${" ".repeat(
+  //       spacingPhone
+  //     )} | ${contacts[index].birthdate}`);
+  //   }
+}
+
 // Main program
-// addContact(contacts, {
-//   id: 6,
-//   fullName: "Frank Taylor",
-//   company: "Logistics Hub",
-//   email: "frank.taylor@logisticshub.com",
-//   phone: "+62 678-901-2345",
-//   dob: "1988-11-11",
-// });
+addContact(dataContacts, {
+  fullName: "Frank Taylor",
+  company: "Logistics Hub",
+  email: "frank.taylor@logisticshub.com",
+  phone: "+62 678-901-2345",
+  birthdate: "1988-11-11",
+});
 
-// findContactById(contacts, 3);
+findContactById(dataContacts, 2491);
 
-// removeContactById(contacts, 3);
+removeLastContact(dataContacts);
+removeLastContact(dataContacts);
+removeLastContact(dataContacts);
+removeLastContact(dataContacts);
+removeLastContact(dataContacts);
+removeLastContact(dataContacts);
 
-// displayContacts(contacts);
+displayContacts(dataContacts);
